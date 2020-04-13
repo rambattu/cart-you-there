e009051d8b7443b25ee895731e274359690f509d
sivarambattu@gmail.com <sbattu@juniper.net>
2020-04-12 19:58:32 -0700

    first cut

---
+++ README.md
@@ -0,0 +1,128 @@
+A Python program using Selenium webdriver API to automate searching for instacart 
+delivery or pick-up availability.
+
+---
+
+#### Developed and Tested on:
+- macOS Mojave **(10.14.6)**
+- Python **3.7.2**
+
+---
+
+## Setup
+
+### Clone repo
+```
+git clone https://github.com/rambattu/cart-you-there.git
+```
+
+### Install Chrome webdriver
+- Based on the version of your Chrome, please pick up the relevant chrome webdriver
+zip file from here:  
+https://sites.google.com/a/chromium.org/chromedriver/downloads
+
+- Unzip and place the binary `chromedriver` in the same directory as the repo
+
+### Install python modules
+
+```
+$ virtualenv env
+$ pip install selenium
+$ pip install PyYAML
+```
+
+```
+$ pip list
+Package    Version
+---------- -------
+pip        20.0.2
+PyYAML     5.3.1
+selenium   3.141.0
+setuptools 46.1.3
+urllib3    1.25.8
+wheel      0.34.2
+```
+
+---
+
+
+## Settings
+If you are using this program, I assume you already have an Instacart account,
+if not please create one before using this.  
+
+#### Instacart deails
+Please update your Instacart account details in [settings.yaml](settings.yaml) file in your local copy
+- Replace FANCY_CART_ACCT@EMAIL.COM with your Instacart email id
+- Replace SUPER_CRYPTO_PWD with your Instacart password  
+
+#### Email details 
+If you would like to get notified via email when there is an availability, please fill
+all 3 sections SENDER_GMAIL_ID, SENDER_GMAIL_PWD, RECEIVER_GMAIL_ID.
+SENDER_GMAIL_ID & RECEIVER_GMAIL_ID can be same.  
+You will not receive email if any of these 3 fields are missing.  
+
+**For this service to work, you have to relax the security setting for your Gmail account
+to allow "Less secure app access", it can be found here https://myaccount.google.com/u/0/lesssecureapps?hl=en. **  
+
+I've only tried with Gmail, not sure if other email services work at this point.
+
+#### Shops
+
+By default, I've included 3 shops Safeway, Target, Sprouts to search for delivery or pickup times.
+You should be able to extend or modify this list with other shops.
+To figure out the shop string to use, you can try the following approach:  
+
+
+Login to your Instacart account and browse to the store you would like to search info for:
+Use the value that is embedded after "store/" and brefore "/info" in the URL.  
+
+Ex:-  
+- Safeway - https://www.instacart.com/store/safeway/info?tab=delivery [safeway]
+- Target - https://www.instacart.com/store/target/info?tab=delivery [target]
+- Harris Teeter - https://www.instacart.com/store/harris-teeter/info?tab=delivery [harris-teeter]
+
+#### Open Browser
+If this setting has value `1`, running will spin up a new instance of Chrome browser
+and launches the search activity in that, when it is `0`, no new browser intance gets
+kicked off, things will run in the background(in headless mode).
+
+---
+
+## Run the program
+```
+$ python find_me_slot.py
+```
+
+This program keeps running upto 8 hours if it can't find any of the shops listed
+in the settings with Delivery or Pickup options.  
+If there is a slot open in any of the shops, 
+it will print it out to the terminal, and **optionally** sends email
+with the details and exits after that.
+
+---
+
+## NOTES
+- To be reasonable without overwhelming too much of resources at this time of need, 
+I chose 3 minutes interval to keep looking for availability in all listed shops.
+This goes on for roughly eight hours and the program quits if it can't find any
+after that. You can start again after that.
+
+- I've tested only limited number of times during this exercise, I am not sure if
+there are any norms in support or against this approach of browsing.
+
+- Use at your own judgement and please be kind.
+
+---
+
+#### Miscellaneous notes and references used during development of this project
+- Started with this https://realpython.com/modern-web-automation-with-python-and-selenium/
+    - But had problem with firefox on my machine with headless option
+- Switched to this document https://linuxhint.com/browser_automation_selenium_python/
+    - Tried with Chrome
+    - I had Chrome 80.0 so downloaded the same version from here https://sites.google.com/a/chromium.org/chromedriver/downloads
+- For email, We have relax Gmail security settings
+    - https://gist.github.com/alexle/1294495#gistcomment-3001681
+    - https://myaccount.google.com/u/0/lesssecureapps?hl=en
+        [Less secure app access]
+- Selenium Python API reference https://selenium-python.readthedocs.io/getting-started.html
+- Exception handling http://allselenium.info/how-to-handle-exceptions-in-selenium-python-webdriver/
---
+++ find_me_slot.py
@@ -0,0 +1,212 @@
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.chrome.options import Options
+
+import re
+import smtplib
+import time
+import yaml
+
+SETTINGS_FILE = "settings.yaml"
+
+cfg = {}
+with open(SETTINGS_FILE,"r") as yamlf:
+    cfg = yaml.load(yamlf, Loader=yaml.SafeLoader)
+
+OPEN_BROWSER = cfg['OPEN_BROWSER']
+CHROMEDRIVER = "chromedriver"
+
+MAIN_SITE = "https://instacart.com"
+SHOP_SITE = "https://www.instacart.com/store/{}/info?tab=delivery"
+
+LOGIN_BUTTON = "//button"
+SITE_LOGIN_BUTTON = '//button[@type="submit"]'
+EMAIL_FIELD_ID = "nextgen-authenticate.all.log_in_email"
+PASSWD_FIELD_ID = "nextgen-authenticate.all.log_in_password"
+DELIVERY_UNAVAIL_FIELD = '//h1'
+DELIVERY_AVAIL_FIELD = '//div/div/div/div[@tabindex="-1"]'
+DELIVERY_UNAVAILABLE_MSG = 'No delivery times available'
+PICKUP_UNAVAILABLE_MSG = 'No pickup times available'
+
+SHOPS = cfg['SHOPS']
+
+INSTACART_EMAIL = cfg['INSTACART_EMAIL']
+INSTACART_PASSWD = cfg['INSTACART_PASSWD']
+SENDER_GMAIL_ID = cfg['SENDER_GMAIL_ID']
+SENDER_GMAIL_PWD = cfg['SENDER_GMAIL_PWD']
+RECEIVER_GMAIL_ID = cfg['RECEIVER_GMAIL_ID']
+
+GMAIL_SMTP = 'smtp.gmail.com'
+GMAIL_SMTP_PORT = 587
+
+WAIT_FOR_MAIN_SITE = 5
+WAIT_FOR_LOGIN_PAGE = 5
+WAIT_FOR_INPUT = 2
+WAIT_FOR_LOGGING_IN = 10
+WAIT_FOR_NEXT_SHOP = 5
+WAIT_FOR_SHOP_LOAD = 10
+
+ROUND_WAIT_TIME = 3*60 # 3 minutes
+ROUND_COUNT = 160 # Roughly 8 hours
+
+class BrowseForMe:
+
+    def __init__(self):
+        opts = Options()
+        if OPEN_BROWSER == 0:
+            opts.headless = True
+        self.browser = webdriver.Chrome(CHROMEDRIVER, options=opts)
+
+
+    def login(self):
+        self.browser.get(MAIN_SITE)
+        time.sleep(WAIT_FOR_MAIN_SITE)
+
+        login_btn = self.browser.find_element_by_xpath(LOGIN_BUTTON)
+        if not login_btn:
+            print("FAILED TO LOCATE LOGIN BUTTON ON MAIN PAGE")
+            self.close_and_quit()
+        login_btn.click()
+        time.sleep(WAIT_FOR_LOGIN_PAGE)
+
+        email_field =self.browser.find_element_by_id(EMAIL_FIELD_ID)
+        if not email_field:
+            print("FAILED TO LOCATE EMAIL FIELD ON LOGIN PAGE")
+            self.close_and_quit()
+        email_field.send_keys(INSTACART_EMAIL)
+        time.sleep(WAIT_FOR_INPUT)
+
+        passwd_field =self.browser.find_element_by_id(PASSWD_FIELD_ID)
+        if not passwd_field:
+            print("FAILED TO LOCATE PASSWORD FIELD ON LOGIN PAGE")
+            self.close_and_quit()
+        passwd_field.send_keys(INSTACART_PASSWD)
+        time.sleep(WAIT_FOR_INPUT)
+
+        login_btn =self.browser.find_element_by_xpath(SITE_LOGIN_BUTTON)
+        if not login_btn:
+            print("FAILED TO LOCATE LOGIN BUTTON ON MAIN PAGE")
+            self.close_and_quit()
+        login_btn.click()
+        time.sleep(WAIT_FOR_LOGGING_IN)
+
+
+    def reload_page(self,shop):
+        time.sleep(WAIT_FOR_SHOP_LOAD)
+        self.browser.get(SHOP_SITE.format(shop))
+        time.sleep(WAIT_FOR_SHOP_LOAD)
+
+
+    def check_availability(self):
+        delivery_available_maybe = False
+        try:
+            message = \
+            self.browser.find_element_by_xpath(DELIVERY_UNAVAIL_FIELD).text
+        except NoSuchElementException as exception:
+            # If this <h1> tag isn't available on page I am assuming there might
+            # be delivery times available
+            delivery_available_maybe = True
+        
+        # Leaving this commented block in case if we have distinguish
+        # the type of unavailability No delivery / No pickups we can use it.
+        # if not delivery_available_maybe:
+        #     match = re.search(DELIVERY_UNAVAILABLE_MSG, message)
+        #     # Some stores have pick up only(Sprouts),
+        #     # check for that message as well
+        #     if not match:
+        #         match = re.search(PICKUP_UNAVAILABLE_MSG, message)
+
+        #     if match:
+        #         print("Not available")
+        #     else:
+        #         print("Delivery available")
+        #     continue
+        return delivery_available_maybe
+
+
+    def check_times(self):
+        available_elems = None
+        try:
+            available_elems = \
+            self.browser.find_elements_by_xpath(DELIVERY_AVAIL_FIELD)
+        except NoSuchElementException as exception:
+            print("CANNOT GET DELIVERY TIMES, WILL TRY AGAIN")
+            return None
+
+        if not available_elems:
+            print("CANNOT GET DELIVERY TIMES, WILL TRY AGAIN")
+            return None
+
+        message = ""
+        for elem in available_elems:
+            message += elem.text
+            message += "\n"
+
+        return message
+
+
+    def send_email(self, message):
+        if cfg['SENDER_GMAIL_ID'] and cfg['SENDER_GMAIL_PWD'] and \
+            cfg['RECEIVER_GMAIL_ID']:
+            self.email(message)
+
+
+    def check_all_stores(self):
+        for shop in SHOPS:
+            self.reload_page(shop)
+
+            if not self.check_availability():
+                # If not available keep reload and keep checking
+                self.send_email("Not there")
+                print("Not there")
+                continue
+
+            message =  self.check_times()
+            if not message:
+                # Unable to figure out times, lets keep trying
+                continue
+
+            print(message)
+            self.send_email(message)
+
+            self.close_and_quit()
+
+
+    def close_and_quit(self):
+        self.browser.close()
+        quit()
+
+
+    def loop_till_you_shop(self):
+
+        for round in range(ROUND_COUNT):
+            self.check_all_stores()
+            print("Round :", round+1)
+            time.sleep(ROUND_WAIT_TIME)
+
+
+    def email(self, message):
+        # establishing connection to the gmail server with domain name and port. 
+        # This differs with each email provider
+        connection = smtplib.SMTP(GMAIL_SMTP,GMAIL_SMTP_PORT)
+        
+        # say hello to the server
+        connection.ehlo()
+        
+        # starting encrypted TLS connection
+        connection.starttls()
+        
+        # log into gmail server with your main address and password
+        connection.login(SENDER_GMAIL_ID, SENDER_GMAIL_PWD)
+        
+        # sending mail to yourself informing you about the price of camera
+        connection.sendmail(SENDER_GMAIL_ID, RECEIVER_GMAIL_ID, message)
+        
+        # ending connection
+        connection.quit()
+
+if __name__ == '__main__':
+    browse = BrowseForMe()
+    browse.login()
+    browse.loop_till_you_shop()
+
